3f6c28ef297cc474d525461ff53ef543a478e258
erika <erika24si@mahasiswa.pcr.ac.id>
2026-03-24 19:45:53 +0700

    Tugas-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,13 @@
+import './custom.css';
+import BiodataDiri from './tugas-2/BiodataDiri';
+
+function App() {
+  return <BiodataDiri />;
+}
+
+export default App;
+
+/*
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -33,3 +43,5 @@ function App() {
 }
 
 export default App
+*/
+
---
+++ src/tugas-2/Foto.jsx
@@ -0,0 +1,10 @@
+function Foto() {
+  return (
+    <img 
+      src="https://i.pinimg.com/564x/ec/d0/1a/ecd01af653e9988359ef441c1ab9ba7a.jpg" 
+      alt="foto" 
+      className="foto"
+    />
+  );
+}
+export default Foto;
