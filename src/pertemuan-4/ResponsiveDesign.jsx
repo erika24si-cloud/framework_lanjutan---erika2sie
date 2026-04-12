0efbd83d79e66f52691d9d2aeddd04d726504112
erika <erika24si@mahasiswa.pcr.ac.id>
2026-04-12 09:51:11 +0700

    tugas-4

--- src/pertemuan-4/ResponsiveDesign.jsx
+++ src/pertemuan-4/ResponsiveDesign.jsx
@@ -1,14 +1,16 @@
 export default function ResponsiveText(){
     return (
         <div>
-        <p className="text-sm md:text-lg lg:text-xl xl:text-2xl mb-4">
+            <p className="text-sm md:text-lg lg:text-xl xl:text-2xl mb-4">
             Coba lakukan zoom in atau zoom out. Perhatikan bahwa ukuran teks akan menyesuaikan dengan ukuran layar.<br/>
             Coba hapus class yang menggunakan prefix breakpoint (md:xxx, lg:xxx, xl:xxx) dan lihat perbedaannya!
+            <ResponsiveWidth />
+            <ResponsiveLayout />
         </p>
-        <ResponsiveWidth/>
-        <ResponsiveLayout/>
-
+        
         </div>
+
+        
     )
 }
 
@@ -23,7 +25,7 @@ function ResponsiveWidth(){
             </p>
             <p>
                 <strong>md:flex-row</strong> pada div parent membuat dua kolom ini sejajar secara horizontal pada layar tablet ke atas,<br/> 
-                sedangkan pada layar lebih kecil (mobile), kolom akan tersusun vertikal secara default (`flex-col`).
+                sedangkan pada layar lebih kecil (mobile), kolom akan tersusun vertikal secara default (flex-col).
             </p>
             
             <div className="flex flex-col md:flex-row mb-4">
